5c1f869affb402ec49c79c4694ec4814d72b9515
John Thomas Shaw Valanidas <john.valanidas@gmail.com>
2023-09-22 19:58:27 -0400

    canvas

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,18 +1,63 @@
 use crate::tuple::Tuple;
-
+use std::fs::File;
 
 #[cfg(test)]
-#[path ="./tests/canvas.rs"] mod canvas;
+#[path = "./tests/canvas.rs"]
+mod canvas;
+
+static PPM_VERSION: &str = "P3";
+static MAX_COLOR_VALUE: u8 = 255;
 
 pub struct Canvas {
-    grid: Vec<Vec<Tuple>> 
+    grid: Vec<Vec<Tuple>>,
 }
 
 impl Canvas {
     pub fn new(w: usize, h: usize) -> Canvas {
-        return Canvas {grid: vec![vec![Tuple::color(0., 0., 0.); w]; h]};
+        return Canvas {
+            grid: vec![vec![Tuple::color(0., 0., 0.); h]; w],
+        };
     }
+
     pub fn write_pixel(&mut self, x: usize, y: usize, pixel: Tuple) {
-        self.grid[x][y] = pixel; 
+        self.grid[x][y] = pixel;
+    }
+
+    pub fn write_canvas_to_ppm_file(&mut self, filename: &str) {
+        let file_data = format!("{}{}", self.create_ppm_header(), self.create_ppm_body());
+    }
+
+    fn create_ppm_header(&self) -> String {
+        let header = format!(
+            "{}\n{} {}\n{}\n",
+            PPM_VERSION,
+            self.grid.len(),
+            self.grid[0].len(),
+            MAX_COLOR_VALUE
+        );
+        return header;
+    }
+    fn create_ppm_body(&self) -> String {
+        let mut body: String = String::new();
+        let mut cell_count: u16 = 0;
+        for row in self.grid {
+            for cell in row {
+                cell_count += 1;
+                let r = (cell.x * MAX_COLOR_VALUE as f32).clamp(0., MAX_COLOR_VALUE as f32) as u8;
+                let g = (cell.y * MAX_COLOR_VALUE as f32).clamp(0., MAX_COLOR_VALUE as f32) as u8;
+                let b = (cell.z * MAX_COLOR_VALUE as f32).clamp(0., MAX_COLOR_VALUE as f32) as u8;
+                let data = format!("{} {} {}", r, g, b);
+                body.push_str(&data.to_string());
+                if cell_count != 0 && cell_count % 5 == 0 {
+                    body.push('\n');
+                } else {
+                    body.push(' ');
+                }
+            }
+        }
+        if body.chars().last().unwrap() != '\n' {
+            body.push('\n');
+        }
+        return body;
     }
 }
--- src/tests/canvas.rs
+++ src/tests/canvas.rs
@@ -5,12 +5,11 @@ use crate::tuple::Tuple;
 mod tests {
     use super::*;
 
-
     #[test]
     fn test_canvas() {
         let canvas = Canvas::new(20,40);
-        assert_eq!(canvas.grid[0].len(), 20);
-        assert_eq!(canvas.grid.len(), 40);
+        assert_eq!(canvas.grid[0].len(), 40);
+        assert_eq!(canvas.grid.len(), 20);
 
         for row in canvas.grid {
             for cell in row {
@@ -28,5 +27,30 @@ mod tests {
         canvas.write_pixel(4, 10, pixel);
 
         assert_eq!(canvas.grid[4][10], pixel);
+    }
+    #[test]
+    fn test_ppm_file_header() {
+        let canvas = Canvas::new(5,3);
+        let data = canvas.create_ppm_header();
+        assert_eq!("P3\n5 3\n255\n", data);
+    }
+    #[test]
+    fn test_ppm_body() {
+        let mut canvas = Canvas::new(5,3);
+        let pixel1  = Tuple::color(1.5,0.,0.);
+        let pixel2  = Tuple::color(0., 0.5,0.);
+        let pixel3  = Tuple::color(-0.5,0.,1.);
+        canvas.write_pixel(0, 0, pixel1);
+        canvas.write_pixel(2, 1, pixel2);
+        canvas.write_pixel(4, 2, pixel3);
+        let data = canvas.create_ppm_body();
+        assert_eq!("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 127 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 255\n", data);
+    }
+    #[test]
+    fn test_ppm_body_ends_with_newline() {
+        let canvas = Canvas::new(100,500);
+        let data = canvas.create_ppm_body();
+        println!("{}", data);
+        assert_eq!('\n', data.chars().last().unwrap());
     }
 }
